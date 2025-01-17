fc20553778518c4a42ff2027e7673fe5d98706a0
fr7mena <fr7mena@gmail.com>
2025-01-17 19:41:19 +0100

    Primer commit: Subida inicial del proyecto

---
+++ script.js
@@ -0,0 +1,46 @@
+
+function comenzar() {
+    const parrafo1 = document.getElementById('parrafo1');
+    const parrafos = document.getElementsByTagName("p");
+    const parrafo2 = parrafos[1]; //Con esto estoy cogiendo el segundo párrafo.
+    const parrafo3 = document.getElementsByClassName("miClase");
+    const boton1 = document.getElementById('cambiarTextos');
+    const boton2 = document.getElementById('cambiarClases');
+    const boton3 = document.getElementById('quitarClases');
+    const boton4 = document.getElementById('quitarClasesAll');
+
+    boton1.addEventListener('click', cambiarTextos); //Si tiene acceso a las variables de arriba porque está llamando a la función en el mismo bloque donde están declaradas.
+    boton2.addEventListener('click', cambiarClases);
+    boton3.addEventListener('click', quitarClases);
+    boton4.addEventListener('click', quitarClasesAll);
+}
+
+function cambiarTextos() {
+    parrafo1.textContent = "Primer párrafo cambiado";
+    parrafo2.textContent = "Segundo párrafo cambiado";
+    parrafo3.textContent = "Tercer párrafo cambiado";
+}
+
+function cambiarClases() {
+    parrafo1.setAttribute("class", "miClase");
+    parrafo2.className = "miOtraClase";
+}
+
+function quitarClases() {
+    parrafo1.setAttribute("class", "");
+    parrafo2.className = "";
+    parrafo3.className = "";
+}
+
+function quitarClasesAll() {
+    parrafos = document.getElementsByTagName("p");
+    for (let i = 0; i < parrafos.length; i++) {
+        if (parrafos[i].className !== "") {
+            parrafos[i].className = "";
+        } else {
+            parrafos[i].className = "miOtraClase";
+        }
+    }
+}
+
+addEventListener('load', comenzar);
